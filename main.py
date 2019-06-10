4e955b01947d9958324c1df11414064bbaca131f
Erick Pinos <erick@erickpinos.com>
2019-06-10 00:54:46 -0400

    changed get_prices to print a dictionary

--- main.py
+++ main.py
@@ -1,53 +1,47 @@
 from pycoingecko import CoinGeckoAPI
 import datetime
+import time
 
 cg = CoinGeckoAPI()
 
-
+# Useful functions
 #bitcoin = cg.get_price(ids='bitcoin', vs_currencies='usd')
-def get_prices(coin,start_date,end_date):
-    coin = 'bitcoin'
-    date = '01-06-2019'
 
-    day = 1
-    num_of_days = 10
+# Pull prices for a cryptocurrency from CoinGecko
+def get_prices(coin,start_date_str,end_date_str):
 
-    datetime_start_date = datetime.datetime.strptime(start_date, '%d-%m-%Y')
-    datetime_end_date = datetime.datetime.strptime(end_date, '%d-%m-%Y') 
-
-    datetime_1 = datetime_start_date
+    start_date = datetime.datetime.strptime(start_date_str, '%d-%m-%Y')
+    end_date = datetime.datetime.strptime(end_date_str, '%d-%m-%Y') 
+    date = start_date
     
-    #coin_info = cg.get_coin_history_by_id(coin, start_date)
+    # Construct the dictionary
+    print(coin + '_price = {')
 
-    while datetime_1 != datetime_end_date + datetime.timedelta(days=1):
-        print(datetime_1.strftime("%d-%m-%Y"))
-        print("- Day", day)
-        print("- Portfolio value is", balance)
-        datetime_string = datetime_1.strftime("%d-%m-%Y")
+    while date != end_date + datetime.timedelta(days=1):
+        date_str = date.strftime("%d-%m-%Y")
 
-        coin_info = cg.get_coin_history_by_id(coin, datetime_string)
-        print(coin, " price on ", datetime_1, " was ", coin_info.get('market_data').get('current_price').get('usd'))
+        coin_info = cg.get_coin_history_by_id(coin, date_str)
+        current_price = coin_info.get('market_data').get('current_price').get('usd')
+        print("    '" + date_str + "': " + format(current_price) + ',')
 
-        datetime_1 = datetime_1 + datetime.timedelta(days=1)
-        day += 1
+        date += datetime.timedelta(days=1)
+        time.sleep(1)
+ 
+    print("}")
 
-        # bitcoin_value = bitcoin_price * bitcoin_held
-    # Iterate & print the dict
-    #for key in coin_info:
-    #    print(key, " = ", bitcoin.get(key))
-
-
+# Iterate & print the dict
+#for key in coin_info:
+#    print(key, " = ", bitcoin.get(key))
 
 balance = 1000
 start_date = '01-06-2019'
 end_date = '09-06-2019'
 
-print("Starting balance is", balance, "USD")
-
 day = 1
 num_of_days = 10
 
-bitcoin_price = { '01-06-2019': 8575.646353354166,
+bitcoin_price = {
+                  '01-06-2019': 8575.646353354166,
                   '02-06-2019': 8554.261728092017,
                   '03-06-2019': 8743.705469134235,
                   '04-06-2019': 8173.632761796738,
@@ -55,7 +49,7 @@ bitcoin_price = { '01-06-2019': 8575.646353354166,
                   '06-06-2019': 7812.448854732150,
                   '07-06-2019': 7821.123897094859,
                   '08-06-2019': 8036.108159122215,
-                  '09-06-2019': 7705.539284064289
+                  '09-06-2019': 7705.539284064289,
                 }
 
 x = datetime.datetime.now()
@@ -146,19 +140,19 @@ def run(coin_1,percentage_1,coin_2,percentage_2,start_date,end_date, strategy):
     return balance
 
 x = 'bitcoin'
-y = '01-06-2019'
+y = '08-06-2019'
 z = '09-06-2019'
-#get_prices(x,y,z)
+get_prices(x,y,z)
 
-strategy_1 = run('usd',1,'bitcoin',0,y,z, 'bnh')
-strategy_2 = run('usd',0,'bitcoin',1,y,z, 'bnh')
-strategy_3 = run('usd',.5,'bitcoin',.5,y,z, 'bnh')
-strategy_4 = run('usd',.5,'bitcoin',.5,y,z, 'rebalance_daily')
+#strategy_1 = run('usd',1,'bitcoin',0,y,z, 'bnh')
+#strategy_2 = run('usd',0,'bitcoin',1,y,z, 'bnh')
+#strategy_3 = run('usd',.5,'bitcoin',.5,y,z, 'bnh')
+#strategy_4 = run('usd',.5,'bitcoin',.5,y,z, 'rebalance_daily')
 
-print('buy and hold usd', strategy_1)
-print('buy and hold btc', strategy_2)
-print('buy and hold 50% usd and 50% btc', strategy_3)
-print('rebalance 50% usd and 50% btc', strategy_4)
+#print('buy and hold usd', strategy_1)
+#print('buy and hold btc', strategy_2)
+#print('buy and hold 50% usd and 50% btc', strategy_3)
+#print('rebalance 50% usd and 50% btc', strategy_4)
 
 
 #Buy and hold strategy
